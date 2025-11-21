6d0e867fcdf0e9bf9f7ca1672145b10ba5b525a4
Matthew Sebastian <22g40009@student.unika.ac.id>
2025-11-21 09:33:27 +0700

    Menambahkan index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="X-UA-Compatible" content="">
+    <title>Coba Git remote</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>lorem</p>
+    <p>loremmmmmmm</p>
+</body>
+</html>
